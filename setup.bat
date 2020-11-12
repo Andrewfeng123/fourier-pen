9f3392e1d58118f6a663d24da29a3a862d9b3fcf
Yvonne Yang <yvonne.y.yang@outlook.com>
2020-11-12 00:24:58 -0500

    added imgui opengl sdl stuff

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
 git clone https://github.com/microsoft/vcpkg.git
 call .\vcpkg\bootstrap-vcpkg.bat
-
-call .\vcpkg\vcpkg.exe install sdl2:x64-windows
+.\vcpkg\vcpkg.exe install sdl2:x64-windows
+.\vcpkg\vcpkg.exe install imgui[sdl2-binding]:x64-windows

b2074916140623fa23fc27a0c5dccd2211a4c6a9
Yvonne Yang <yvonne.y.yang@outlook.com>
2021-02-19 10:00:12 -0500

    reformatted function open braces to be on the same line

--- src/processing.cpp
+++ src/processing.cpp
@@ -2,45 +2,40 @@
  *
  * */
 #include "processing.h"
-#include "graphics.h"
-#include "globals.h"
 
-#include <vector>
 #include <cmath>
 #include <iostream>
+#include <vector>
+
+#include "globals.h"
+#include "graphics.h"
 
 std::ostream &operator<<(std::ostream &os, const struct Coord &coord);
 
 /* DTFS examples */
 std::vector<Coord> square = {{0.5, 0.5}, {0.5, -0.5}, {-0.5, 0.5}, {-0.5, -0.5}};
 
-Coord_list_t sampleSVG()
-{
+Coord_list_t sampleSVG() {
     return {};
 }
 
-alpha_list_t DTFS(const Coord_list_t &coords, int numCircles)
-{
-    if (numCircles <= 0)
-    {
+alpha_list_t DTFS(const Coord_list_t &coords, int numCircles) {
+    if (numCircles <= 0) {
         std::cerr << "Error in DTFS: numCircles not positive." << std::endl;
         return {};
     }
-    if (coords.size() == 0)
-    {
+    if (coords.size() == 0) {
         std::cerr << "Error in DTFS: empty coord list." << std::endl;
         return {};
     }
 
-    const int N = (int) coords.size(); // num of points
-    const int K = (int) numCircles;
+    const int N = (int)coords.size();  // num of points
+    const int K = (int)numCircles;
     const float omega = 2 * M_PI / N;
     alpha_list_t alpha(K);
-    for (int k = 0; k < K; k++)
-    {
+    for (int k = 0; k < K; k++) {
         alpha[k] = std::make_pair(0.0f, 0.0f);
-        for (int n = 0; n < N; n++)
-        {
+        for (int n = 0; n < N; n++) {
             alpha[k].first += coords[n].x * cos(-omega * k * n) - coords[n].y * sin(-omega * k * n);
             alpha[k].second += coords[n].y * cos(-omega * k * n) + coords[n].x * sin(-omega * k * n);
         }
@@ -51,8 +46,7 @@ alpha_list_t DTFS(const Coord_list_t &coords, int numCircles)
     return alpha;
 }
 
-void drawFrame(alpha_list_t alphas, int time)
-{
+void drawFrame(alpha_list_t alphas, int time) {
     // draw a circle
     struct Coord centre;
     //drawCircle(centre, );
@@ -71,8 +65,7 @@ void drawFrame(alpha_list_t alphas, int time)
 }*/
 
 // for debugging
-std::ostream &operator<<(std::ostream &os, const struct Coord &coord)
-{
+std::ostream &operator<<(std::ostream &os, const struct Coord &coord) {
     os << "(" << coord.x << "," << coord.y << ')';
     return os;
 }

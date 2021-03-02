c3caa5145f0ba2f789e6627989d0e729a0ddc992
Yvonne Yang <yvonne.y.yang@outlook.com>
2021-03-02 15:43:20 -0500

    wrote drawFrame method and created tests

--- src/processing.cpp
+++ src/processing.cpp
@@ -9,17 +9,15 @@
 
 #include "globals.h"
 #include "graphics.h"
+//#include "samples.h"
 
 std::ostream &operator<<(std::ostream &os, const struct Coord &coord);
-
-/* DTFS examples */
-std::vector<Coord> square = {{0.5, 0.5}, {0.5, -0.5}, {-0.5, 0.5}, {-0.5, -0.5}};
 
 Coord_list_t sampleSVG() {
     return {};
 }
 
-alpha_list_t DTFS(const Coord_list_t &coords, int numCircles) {
+alpha_list_t DTFS(const Coord_list_t &coords, const int numCircles) {
     if (numCircles <= 0) {
         std::cerr << "Error in DTFS: numCircles not positive." << std::endl;
         return {};
@@ -30,39 +28,60 @@ alpha_list_t DTFS(const Coord_list_t &coords, int numCircles) {
     }
 
     const int N = (int)coords.size();  // num of points
-    const int K = (int)numCircles;
+    const int K = (numCircles <= N) ? numCircles : N;
+    if (numCircles > coords.size()) {  ///////////// output something to the user?
+        std::cout << "Not enough points for that many circles!" << std::endl;
+    }
     const float omega = 2 * M_PI / N;
     alpha_list_t alpha(K);
     for (int k = 0; k < K; k++) {
         alpha[k] = std::make_pair(0.0f, 0.0f);
         for (int n = 0; n < N; n++) {
-            alpha[k].first += coords[n].x * cos(-omega * k * n) - coords[n].y * sin(-omega * k * n);
-            alpha[k].second += coords[n].y * cos(-omega * k * n) + coords[n].x * sin(-omega * k * n);
+            alpha[k].first += coords[n].x * cosf(-omega * k * n) - coords[n].y * sinf(-omega * k * n);
+            alpha[k].second += coords[n].y * cosf(-omega * k * n) + coords[n].x * sinf(-omega * k * n);
         }
         alpha[k].first /= N;
         alpha[k].second /= N;
+        //if (k%12==0) std::cout << "start: " << alpha[k].first <<  " " << alpha[k].second << std::endl;
     }
 
     return alpha;
 }
 
-void drawFrame(alpha_list_t alphas, int time) {
+inline float magnitude(float x, float y) {
+    return sqrtf(x * x + y * y);
+}
+inline float phase(float x, float y) {
+    return atan2f(y, x);
+}
+
+void drawFrame(const alpha_list_t &alphas, const int time) {
+    const float speed = 100.0f;  //////////// fix this later
     // draw a circle
     struct Coord centre;
-    //drawCircle(centre, );
+    for (int k = 0; k < alphas.size(); k++) {
+        auto mag = magnitude(alphas[k].first, alphas[k].second);
+        auto phi = phase(alphas[k].first, alphas[k].second);
+        drawCircle(centre, mag);
+        drawLine({centre, {centre.x + mag * cosf(phi + k * time / speed), centre.y + mag * sinf(phi + k * time / speed)}});
+        centre = {
+            centre.x + mag * cosf(phi + k * time / speed), centre.y + mag * sinf(phi + k * time / speed)};
+    }
 }
 
 // Temporary testing method
-/*int main()
-{
-    auto params = DTFS(square);
+/*
+int main() {
+    auto params = DTFS(square_wave, 200);
+
     // print coord list
     for (auto e : params)
     {
         std::cout << e.first << " + i*" << e.second << std::endl;
     }
     return 0;
-}*/
+}
+*/
 
 // for debugging
 std::ostream &operator<<(std::ostream &os, const struct Coord &coord) {
--- src/processing.h
+++ src/processing.h
@@ -7,10 +7,13 @@
 #include <vector>
 
 #define DEFAULT_NUM_CIRCLES 10
+#define MAX_CIRCLES 100
+#define MIN_CIRCLES 1
 
 typedef std::vector<Coord> Coord_list_t;
 typedef std::vector<std::pair<float, float>> alpha_list_t;
 
 Coord_list_t sampleSVG();
-alpha_list_t DTFS(const Coord_list_t &coords, int numCircles = DEFAULT_NUM_CIRCLES);
-void drawFrame(alpha_list_t alphas, int time);
+alpha_list_t DTFS(const Coord_list_t& coords, const int numCircles = DEFAULT_NUM_CIRCLES);
+void drawFrame(const alpha_list_t& alphas, const int time);
+inline float mag(float x, float y);
